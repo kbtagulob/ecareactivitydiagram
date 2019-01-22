7f4650f6e75b24ddb57f8dda0a4c5ee52ca518f2
kbtagulob <35475112+kbtagulob@users.noreply.github.com>
2019-01-21 18:21:59 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,11 @@
 </head>
 <body>
 <img src="ecareflowchart.png">
+  <p>In my class diagram, the customer needs to have an ID, Name, Address and date of birth. The ID will be an integer, Name is string, adress is also string,mobile number is integer, while the date of birth is date. Now in the applicant side, the applicant needs to have an applicant id, Name, Mobile number, date of birth, educational attainment and years of experience. The applicant's id is integer, name is string, address is string, mobile number is integer, date of birth is date, educational attainment is string, while the years of experience is integer.
+    <br><br>
+    In the applicant's side, I put the educational attainment and years of experience in the database so that the customer will be able to see what they have to offer.
+    <br><br>
+    Now when the customer wants to book an applicant, they just need to click on book button in the app so that they will be able to book an applicant, They need to input their pain level, and added details of what they are feeling. The location will be automatic as gps generated, The id of the customer that will book will also be inputted including their mobile number. The applicant that will be chosen will have the applicant that will be chosen will have the applicant id and mobile number will also be in the booking section.
+  </p>
 </body>
 </html>
